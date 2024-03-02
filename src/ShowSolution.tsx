07dd89e87366935a8da1ea4d03516cba1025c125
Sakhee Desai <sakheed@udel.edu>
2024-03-02 16:36:10 -0500

    fixing bugs

--- src/ShowSolution.tsx
+++ src/ShowSolution.tsx
@@ -1,17 +1,7 @@
 import * as React from "react"
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
   Button,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 
 const ShowSolution: React.FC = () => {
